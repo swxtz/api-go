8ecb915ed7f4079012509c4e557345eb904b665f
Gustavo Mendonça <alf4r6@gmail.com>
2023-07-21 20:21:50 -0300

    feat: created get todo

---
+++ models/get.go
@@ -0,0 +1,19 @@
+package models
+
+import "github.com/swxtz/api-go/db"
+
+func Get(id int64) (todo Todo, err error) {
+	conn, err := db.OpenConnection()
+
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	query := `SELECT * FROM todos WHERE id = $1`
+	row := conn.QueryRow(query, id)
+
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+
+	return
+}
