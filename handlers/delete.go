57f15bb3274e5e4cd52b225254787bba26f7bb3c
Gustavo Mendonça <alf4r6@gmail.com>
2023-07-21 21:03:30 -0300

    feat: created delete handler

---
+++ handlers/delete.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/swxtz/api-go/models"
+)
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Delete(int64(id))
+
+	if err != nil {
+		log.Printf("Erro ao remover registro %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: foram removidos %d registros", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   false,
+		"Message": fmt.Sprintf("Todo removido com sucesso! ID: %d", id),
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
